0de55c294bf2a0dfecc04a5a8995a224ed5e8f68
Uğur Altıntaş <uguraltintas17@gmail.com>
2024-11-19 12:19:16 +0300

    Initial Commit

---
+++ LoodosMovie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoodosMovie
+//
+//  Created by Uğur Altıntaş on 19.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
